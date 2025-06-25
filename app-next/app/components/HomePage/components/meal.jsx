06b3cd1c1140549bbd1352457b038fc457d86950
Vadim Tofan <vad.tofan@gmail.com>
2025-06-25 15:19:45 +0200

    Added Meal component with styling and integrate into last week's MealsList

---
+++ app-next/app/components/HomePage/components/meal.jsx
@@ -0,0 +1,13 @@
+import styles from "./meal.module.css";
+
+export default function Meal({ meal }) {
+  return (
+    <div className={styles.meal}>
+      <h2 className={styles.meal__title}>{meal.title}</h2>
+      <p className={styles.meal__description}>{meal.description}</p>
+      <p className={styles.meal__location}>Location: {meal.location}</p>
+      <p className={styles.meal__maxReservations}>Max Reservations: {meal.maxReservations}</p>
+      <p className={styles.meal__price}>Price: {meal.price}</p>
+    </div>
+  );
+}
