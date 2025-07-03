3c50591f2420867ef32bf0913880e44fe3e398bc
Vadim Tofan <vad.tofan@gmail.com>
2025-07-04 01:09:58 +0200

    refactor: simplify layout structure by removing unnecessary Layout component

--- app-next/app/layout.js
+++ app-next/app/layout.js
@@ -3,27 +3,19 @@ import "./globals.css";
 import Header from "./components/header-footer/header/header.jsx";
 import Footer from "./components/header-footer/footer/footer.jsx";
 
-export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body>
-        <Layout>{children}</Layout>
-      </body>
-    </html>
-  );
-}
-
-export function Layout({ children }) {
-  return (
-    <div className="layout">
-      <Header />
-      <div className="databox">{children}</div>
-      <Footer />
-    </div>
-  );
-}
-
 export const metadata = {
   title: "Meal-Sharing APP",
   description: "This is a Meal-Sharing APP HYF DK",
 };
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        <Header />
+        <div className="databox">{children}</div>
+        <Footer />
+      </body>
+    </html>
+  );
+}
