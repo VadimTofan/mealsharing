a46ad0955a170ffbf07465ebed7c39f769c3603e
Vadim Tofan <vad.tofan@gmail.com>
2025-07-04 13:28:12 +0200

    Fixed Burger menu wrapping issue

---
+++ app-next/app/head.js
@@ -0,0 +1,9 @@
+export default function Head() {
+  return (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <title>Meal-Sharing APP</title>
+      <meta name="description" content="This is a Meal-Sharing APP HYF DK" />
+    </>
+  );
+}
